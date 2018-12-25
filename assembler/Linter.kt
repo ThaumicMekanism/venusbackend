6f81dcf0919e1970b1f0aad2e795fbc785c6f2d2
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-12-25 10:04:54 -0800

    Removed linter jsname tags

--- assembler/Linter.kt
+++ assembler/Linter.kt
@@ -1,6 +1,6 @@
 package venusbackend.assembler
 
-@JsName("LintError") data class LintError(val lineNumber: Int, val message: String, val isError: Boolean = true)
+data class LintError(val lineNumber: Int, val message: String, val isError: Boolean = true)
 
 /**
  * Linter for RISC-V code
@@ -8,14 +8,14 @@ package venusbackend.assembler
  * At the moment this implemented by just running the venusbackend.assembler outright, unless the text is too long.
  * A more intelligent approach would run the linter on each line incrementally.
  */
-@JsName("Linter") object Linter {
+object Linter {
     /**
      * Lints the given text, which is expected to be an entire assembly file
      *
      * @param text the text to lint
      * @fixme this relies on Kotlin using JS array for Array, but it will probably remain that way
      */
-    @JsName("lint") fun lint(text: String): Array<LintError> {
+    fun lint(text: String): Array<LintError> {
         val (_, errors, warnings) = Assembler.assemble(text)
         val linterErrors = ArrayList<LintError>()
         for (error in errors) {
