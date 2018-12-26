f5dde8a7276c08a30d3adb293bcf5b924ecb5788
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-12-26 08:48:52 -0800

    Assembler: Parse code with system-independent line-endings

--- assembler/Assembler.kt
+++ assembler/Assembler.kt
@@ -100,7 +100,7 @@ internal class AssemblerPassOne(private val text: String, name: String = "anonym
     }
 
     private fun doPassOne() {
-        for (line in text.split('\n')) {
+        for (line in text.lines()) {
             try {
                 currentLineNumber++
 
