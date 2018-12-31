23cc1137d5214728da4eb651bd19a0513e620ff6
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-12-31 11:57:54 -0800

    Removed unsigned cast

--- riscv/insts/integer/extensions/compressed/cs/c.subw.kt
+++ riscv/insts/integer/extensions/compressed/cs/c.subw.kt
@@ -1,6 +1,6 @@
 package venusbackend.riscv.insts.integer.extensions.compressed.cs
 
-import venusbackend.numbers.toUQuadWord
+import venusbackend.numbers.toQuadWord
 import venusbackend.riscv.insts.InstructionNotSupportedError
 import venusbackend.riscv.insts.dsl.types.compressed.CSTypeInstruction
 
@@ -11,6 +11,6 @@ val csubw = CSTypeInstruction(
         funct6 = 0b100011,
         eval16 = { a, b -> throw InstructionNotSupportedError("addiw is not supported on 16 bit systems!") },
         eval32 = { a, b -> throw InstructionNotSupportedError("addiw is not supported on 32 bit systems!") },
-        eval64 = { a, b -> (a.toInt() - b.toInt()).toULong().toLong() },
-        eval128 = { a, b -> (a.toInt() - b.toInt()).toUQuadWord() }
+        eval64 = { a, b -> (a.toInt() - b.toInt()).toLong() },
+        eval128 = { a, b -> (a.toInt() - b.toInt()).toQuadWord() }
 )
