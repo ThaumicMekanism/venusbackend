73e2642b264c9be3e50cd1bdafa6bc53adaf7dc9
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-10-20 17:14:40 -0700

    Set default simsettings max steps to -1

--- simulator/SimulatorSettings.kt
+++ simulator/SimulatorSettings.kt
@@ -8,7 +8,7 @@ data class SimulatorSettings(
     var mutableText: Boolean = true,
     var ecallOnlyExit: Boolean = false,
     var setRegesOnInit: Boolean = true,
-    var maxSteps: Int = 500000,
+    var maxSteps: Int = -1,
     var ASLR: Boolean = false,
     var NX_bit: Boolean = false,
     var allowAccessBtnStackHeap: Boolean = false
