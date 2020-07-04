1d20e8888f9457de25953c275e18c6ae52664314
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-07-03 23:13:06 -0700

    Made it a bit more generic

--- utils/SocketMessage.kt
+++ utils/SocketMessage.kt
@@ -1,17 +1,17 @@
 package venusbackend.utils
 
-data class SocketMessage(var v: String, var cmd: String, var data: String="") {
+data class SocketMessage(var v: String, var cmd: String, var data: Any="") {
     companion object {
-        fun parse(data: MutableMap<String, String>): SocketMessage {
+        fun parse(data: MutableMap<String, Any>): SocketMessage {
             return SocketMessage(
                 v = data["v"] as String,
                 cmd = data["cmd"] as String,
-                data = data["data"] as String
+                data = data["data"] ?: ""
             )
         }
     }
 
-    fun mutableMap(): MutableMap<String, String> {
-        return mutableMapOf<String, String>(Pair("v", v), Pair("cmd", cmd), Pair("data", data))
+    fun mutableMap(): MutableMap<String, Any> {
+        return mutableMapOf<String, Any>(Pair("v", v), Pair("cmd", cmd), Pair("data", data))
     }
 }
