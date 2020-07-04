f887ba3a2b418ac5fb2f44db88863ecded53e58c
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-07-03 22:35:38 -0700

    Added parser to the socket message

--- utils/SocketMessage.kt
+++ utils/SocketMessage.kt
@@ -1,5 +1,13 @@
 package venusbackend.utils
 
-data class SocketMessage(var v: String, var cmd: String, var data: Any) {
-
+data class SocketMessage(var v: String, var cmd: String, var data: Any?) {
+    companion object {
+        fun parse(data: MutableMap<String, Any?>): SocketMessage {
+            return SocketMessage(
+                v = data["v"] as String,
+                cmd = data["cmd"] as String,
+                data = data["data"]
+            )
+        }
+    }
 }
