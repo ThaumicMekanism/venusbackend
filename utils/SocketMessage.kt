5b419691e54118f16f2a75ff2811967de84e70ea
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-07-04 00:43:30 -0700

    Generalized parse

--- utils/SocketMessage.kt
+++ utils/SocketMessage.kt
@@ -2,7 +2,7 @@ package venusbackend.utils
 
 data class SocketMessage(var v: String, var cmd: String, var data: Any="") {
     companion object {
-        fun parse(data: MutableMap<String, Any>): SocketMessage {
+        fun parse(data: Map<String, Any>): SocketMessage {
             return SocketMessage(
                 v = data["v"] as String,
                 cmd = data["cmd"] as String,
