e453c3a834f8acea96dde3008cb295d9d95fcca3
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-07-03 22:46:48 -0700

    Return mutable map

--- utils/SocketMessage.kt
+++ utils/SocketMessage.kt
@@ -2,12 +2,16 @@ package venusbackend.utils
 
 data class SocketMessage(var v: String, var cmd: String, var data: String="") {
     companion object {
-        fun parse(data: MutableMap<String, Any?>): SocketMessage {
+        fun parse(data: MutableMap<String, String>): SocketMessage {
             return SocketMessage(
                 v = data["v"] as String,
                 cmd = data["cmd"] as String,
                 data = data["data"] as String
             )
         }
+    }
+
+    fun mutableMap(): MutableMap<String, String> {
+        return mutableMapOf<String, String>(Pair("v", v), Pair("cmd", cmd), Pair("data", data))
     }
 }
