7618c35b6948bd3f137d080b6f9559f29608cfd2
Stephan Kaminsky <thaumicmekanism@gmail.com>
2021-04-21 13:01:00 -0700

    Removed branch distance as it is not needed

--- riscv/insts/dsl/parsers/DoNothingParser.kt
+++ riscv/insts/dsl/parsers/DoNothingParser.kt
@@ -5,8 +5,6 @@ import venusbackend.riscv.MachineCode
 import venusbackend.riscv.Program
 
 object DoNothingParser : InstructionParser {
-    const val B_TYPE_MIN = -4096
-    const val B_TYPE_MAX = 4095
     override operator fun invoke(prog: Program, mcode: MachineCode, args: List<String>, dbg: DebugInfo) {
         checkArgsLength(args.size, 0, dbg)
     }
